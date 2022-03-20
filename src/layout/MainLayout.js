7e61c4410aff626713542ced353ff599018749d4
thomas78125 <thomas78125@gmail.com>
2022-03-20 11:40:17 +0100

    Films Nav

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 const MainLayout = ({children}) => {
     return (
         <>
-        //v-if page sign in
+        {/* v-if page sign in */}
             <Header/>
             <main>
                 {children}
